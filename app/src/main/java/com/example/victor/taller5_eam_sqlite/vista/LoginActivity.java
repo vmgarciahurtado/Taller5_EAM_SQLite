c68b8c813b39e3774ec9941c7ab2820aba7947c0
JU4NG1R4L <juanj8845@gmail.com>
2019-03-28 22:14:47 -0500

    Se realizaron cambios - JU4NG1R4L

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/LoginActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/LoginActivity.java
@@ -2,18 +2,20 @@ package com.example.victor.taller5_eam_sqlite.vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.example.victor.taller5_eam_sqlite.R;
 
 import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
-TextView TitUsuario;
+    EditText txtNombreUsuario, txtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TitUsuario = (TextView) findViewById(R.id.titUsuario);
+        txtNombreUsuario = (EditText) findViewById(R.id.txtNombreUsuario);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
     }
 }
--- app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/MainActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/MainActivity.java
@@ -7,13 +7,12 @@ import android.widget.EditText;
 import com.example.victor.taller5_eam_sqlite.R;
 
 public class MainActivity extends AppCompatActivity {
-EditText txtNombreUsuario, txtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtNombreUsuario = (EditText) findViewById(R.id.txtNombreUsuario);
-        txtPassword = (EditText) findViewById(R.id.txtPassword);
+       ;
     }
     
 }
