b9ba949ef95a20decbc7af53ded50fa61c5d909d
JU4NG1R4L <juanj8845@gmail.com>
2019-03-28 22:05:36 -0500

    Se realizaron cambios - JU4NG1R4L

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/RegistrarUsuarioActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/RegistrarUsuarioActivity.java
@@ -10,8 +10,9 @@ import com.example.victor.taller5_eam_sqlite.R;
 import com.example.victor.taller5_eam_sqlite.controlador.ControladorUsuario;
 
 public class RegistrarUsuarioActivity extends AppCompatActivity {
-EditText txtNombreCompleto, txtUsuario, txtPassword, txtVerPassword;
-ControladorUsuario controlador;
+    EditText txtNombreCompleto, txtUsuario, txtPassword, txtVerPassword;
+    ControladorUsuario controlador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +24,35 @@ ControladorUsuario controlador;
         controlador = new ControladorUsuario(this);
     }
 
-    public void Registrar(View view){
-      String nombreCompleto = txtNombreCompleto.getText().toString();
-      String nombreUsuario = txtUsuario.getText().toString();
-      String password = txtPassword.getText().toString();
-      String verPassword = txtVerPassword.getText().toString();
-      if(password.equals(verPassword)){
+    public void Registrar(View view) {
+        String nombreCompleto = txtNombreCompleto.getText().toString();
+        String nombreUsuario = txtUsuario.getText().toString();
+        String password = txtPassword.getText().toString();
+        String verPassword = txtVerPassword.getText().toString();
+        if (nombreCompleto.equals("") || nombreUsuario.equals("") || password.equals("") || verPassword.equals("")) {
+            Toast.makeText(getApplicationContext(), "Hay campos vacios", Toast.LENGTH_SHORT).show();
+        } else {
+            if (password.equals(verPassword)) {
+                if (controlador.buscarUsuarioRepetido(nombreUsuario) == false) {
+                    if (controlador.guardarUsuario(nombreCompleto, nombreUsuario, password)) {
+                        limpiarCampos();
+                        Toast.makeText(getApplicationContext(), "Los datos se almacenaron correctamente", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Error al almacenar la información, intenta de nuevo", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "El nombre de usuario ya esta en uso", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
-      }else{
-          Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
-      }
+    public void limpiarCampos() {
+        txtNombreCompleto.setText("");
+        txtUsuario.setText("");
+        txtPassword.setText("");
+        txtVerPassword.setText("");
     }
 }
