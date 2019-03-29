68bb45db1d74f04190d96bdce1483b077fd8ea26
victor <vmgarciahurtado@gmail.com>
2019-03-28 19:45:30 -0500

    Se agregaron nuevos cambios a la aplicacion

---
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/DAO/CelularDao.java
@@ -0,0 +1,8 @@
+package com.example.victor.taller5_eam_sqlite.DAO;
+
+/**
+ * Created by victor on 3/28/19.
+ */
+
+public class CelularDao {
+}
---
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/infraestructura/Conexion.java
@@ -0,0 +1,8 @@
+package com.example.victor.taller5_eam_sqlite.infraestructura;
+
+/**
+ * Created by victor on 3/28/19.
+ */
+
+public class Conexion {
+}
