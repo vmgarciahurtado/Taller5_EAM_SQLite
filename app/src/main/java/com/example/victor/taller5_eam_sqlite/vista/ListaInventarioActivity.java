d1a882e91af4ad676e3ee3a94135af42562ff027
victor <vmgarciahurtado@gmail.com>
2019-03-28 20:22:09 -0500

    Se agregaron nuevos cambios a la aplicacion

---
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/ListaInventarioActivity.java
@@ -0,0 +1,15 @@
+package com.example.victor.taller5_eam_sqlite.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.victor.taller5_eam_sqlite.R;
+
+public class ListaInventarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_usuarios);
+    }
+}
