ae6ec6dd695a938a68fc2e626ccc54d706c4d2bf
victor <vmgarciahurtado@gmail.com>
2019-03-28 19:57:41 -0500

    Se agregaron nuevos cambios a la aplicacion

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/modelo/Usuario.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/modelo/Usuario.java
@@ -5,4 +5,38 @@ package com.example.victor.taller5_eam_sqlite.modelo;
  */
 
 public class Usuario {
+    String nombre,usuario,contraseña;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
