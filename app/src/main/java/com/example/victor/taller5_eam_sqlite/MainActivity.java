250e8a0503310f09cd3525b32167f6746e790136
JU4NG1R4L <juanj8845@gmail.com>
2019-03-28 19:11:52 -0500

    Se realizaron cambios - JU4NG1R4L

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/MainActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.victor.taller5_eam_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText txtNombreUsuario, txtPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtNombreUsuario = (EditText) findViewById(R.id.txtNombreUsuario);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
     }
+    
 }
