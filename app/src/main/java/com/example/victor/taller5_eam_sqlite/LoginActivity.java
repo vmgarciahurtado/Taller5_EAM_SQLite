28847a67bc630b12b8898b42509fadfb8b9429b7
JU4NG1R4L <juanj8845@gmail.com>
2019-03-28 19:21:31 -0500

    Se realizaron cambios - JU4NG1R4L

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/LoginActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/LoginActivity.java
@@ -2,12 +2,16 @@ package com.example.victor.taller5_eam_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
-
+TextView txtTitUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txtTitUsuario = (TextView) findViewById(R.id.titUsuario);
     }
 }
