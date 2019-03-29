b26bc9c7c9cb8f98e92f9ba38888d0cf9af9295f
victor <vmgarciahurtado@gmail.com>
2019-03-28 19:52:19 -0500

    Se agregaron nuevos cambios a la aplicacion

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/modelo/Celular.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/modelo/Celular.java
@@ -5,4 +5,58 @@ package com.example.victor.taller5_eam_sqlite.modelo;
  */
 
 public class Celular {
+
+    public Celular(String imei, String marca, String nombre, String usuario, String propietario) {
+        this.imei = imei;
+        this.marca = marca;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.propietario = propietario;
+    }
+
+    public Celular() {
+
+    }
+
+    String imei,marca,nombre,usuario,propietario;
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 }
