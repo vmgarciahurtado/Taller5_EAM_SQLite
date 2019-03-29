6bf2076d42cb430726dd4699437744ff133da2f1
victor <vmgarciahurtado@gmail.com>
2019-03-28 21:27:11 -0500

    Se agregaron nuevos cambios a la aplicacion

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/RegistrarInventarioActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/RegistrarInventarioActivity.java
@@ -2,14 +2,47 @@ package com.example.victor.taller5_eam_sqlite.vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.victor.taller5_eam_sqlite.R;
 
 public class RegistrarInventarioActivity extends AppCompatActivity {
 
+    EditText campoImei,campoNombre;
+    Spinner spinnerMarcar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_inventario);
+
+        campoImei = findViewById(R.id.campoImei);
+        campoNombre = findViewById(R.id.campoNombre);
+        spinnerMarcar = findViewById(R.id.spinnerMarca);
+
     }
+
+    public void guardar(View view) {
+    }
+
+    public void buscar(View view) {
+    }
+
+    public void eliminar(View view) {
+    }
+
+    public void modificar(View view) {
+    }
+
+    public void listar(View view) {
+    }
+
+
+
+
+
+
+
 }
