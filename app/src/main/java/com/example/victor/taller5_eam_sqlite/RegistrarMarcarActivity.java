31ffe28cd3994a4ed75284a3a4d048db07d127b0
JU4NG1R4L <juanj8845@gmail.com>
2019-03-28 19:46:52 -0500

    Se realizaron cambios - JU4NG1R4L

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/RegistrarMarcarActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/RegistrarMarcarActivity.java
@@ -2,12 +2,15 @@ package com.example.victor.taller5_eam_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegistrarMarcarActivity extends AppCompatActivity {
-
+EditText txtNombreMarca, txtDescripcion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_marcar);
+        txtNombreMarca = (EditText) findViewById(R.id.txtNombreMarca);
+        txtDescripcion = (EditText) findViewById(R.id.txtDescripcionMarca);
     }
 }
