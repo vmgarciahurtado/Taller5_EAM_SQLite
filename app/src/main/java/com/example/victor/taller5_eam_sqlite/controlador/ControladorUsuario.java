66d4993504cb11c47e8cc472b235909bcae6d5cb
JU4NG1R4L <juanj8845@gmail.com>
2019-03-28 20:38:18 -0500

    Se realizaron cambios - JU4NG1R4L

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/controlador/ControladorUsuario.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/controlador/ControladorUsuario.java
@@ -1,4 +1,31 @@
 package com.example.victor.taller5_eam_sqlite.controlador;
 
+import android.app.Activity;
+
+import com.example.victor.taller5_eam_sqlite.DAO.UsuarioDAO;
+import com.example.victor.taller5_eam_sqlite.modelo.Usuario;
+
 public class ControladorUsuario {
+    UsuarioDAO dao;
+
+    public ControladorUsuario(Activity activity) {
+        this.dao = new UsuarioDAO(activity);
+    }
+
+    public boolean guardarUsuario(String nombreCompleto, String nombreUsuario, String password){
+        Usuario usuario = new Usuario(nombreCompleto, nombreUsuario, password);
+        return dao.guardar(usuario);
+
+    }
+    public Usuario buscarUsuario(String nombreUsuario){
+        return dao.buscar(nombreUsuario);
+    }
+    public boolean eliminarUsuario(String nombreUsuario){
+        Usuario usuario = new Usuario("", nombreUsuario, "");
+        return dao.eliminar(usuario);
+    }
+    public boolean modificarUsuario(String nombreCompleto, String nombreUsuario, String password){
+        Usuario usuario = new Usuario(nombreCompleto, nombreUsuario, password);
+        return dao.modificar(usuario);
+    }
 }
