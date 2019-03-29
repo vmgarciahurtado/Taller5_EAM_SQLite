a66be7aef96b5d6e8b8e8018f561fe3b3e58c0bb
victor <vmgarciahurtado@gmail.com>
2019-03-28 19:32:38 -0500

    Se crea la estructura del proyecto

--- app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/MainActivity.java
+++ app/src/main/java/com/example/victor/taller5_eam_sqlite/vista/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.victor.taller5_eam_sqlite;
+package com.example.victor.taller5_eam_sqlite.vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.victor.taller5_eam_sqlite.R;
 
 public class MainActivity extends AppCompatActivity {
 
